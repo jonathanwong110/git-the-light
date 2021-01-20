8a99be2fa018f836b4991b16b9bec7244f7d537f
jonathanwong110 <jonathan.wong110@gmail.com>
2021-01-20 02:01:44 -0500

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [darkMode, setDarkMode] = React.useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div
+        id="toggle"
+        onClick={() => darkMode === false ? setDarkMode(true) : setDarkMode(false)}
+      >
+      </div>
     </div>
   );
+
 }
 
-export default App;
+
+export default App
